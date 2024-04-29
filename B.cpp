96713bbbbad2fa0a0f7d38eb9e435e405f952fe0
Samta Jadhav <132902635+SamtaJadhav5802@users.noreply.github.com>
2024-04-29 08:53:17 +0530

    Add files via upload

---
+++ B.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<stdlib.h>
+#include<omp.h>
+using namespace std;
+
+void bubble(int *, int);
+void swap(int &, int &);
+
+
+void bubble(int *a, int n)
+{
+    for(  int i = 0;  i < n;  i++ )
+     {  	 
+   	 int first = i % 2; 	 
+
+     
+   	 #pragma omp parallel sections num_threads(8) for shared(a,first)
+   	 for(  int j = first;  j < n-1;  j += 2  )
+   	   {  	 
+   		 if(  a[ j ]  >  a[ j+1 ]  )
+   		  {  	 
+     			 swap(  a[ j ],  a[ j+1 ]  );
+   		  }  	 
+   		   }  	 
+     }
+}
+
+
+void swap(int &a, int &b)
+{
+
+    int test;
+    test=a;
+    a=b;
+    b=test;
+
+}
+
+int main()
+{
+
+    int *a,n;
+    double start_time, end_time;
+    cout<<"\n enter total no of elements=>";
+    cin>>n;
+    a=new int[n];
+    cout<<"\n enter elements=>";
+    for(int i=0;i<n;i++)
+    {
+   	 cin>>a[i];
+    }
+    
+    start_time = omp_get_wtime(); // Start measuring time
+    
+    bubble(a,n);
+    
+    end_time = omp_get_wtime(); // End measuring time
+
+    
+    cout<<"\n sorted array is=>";
+    for(int i=0;i<n;i++)
+    {
+   	 cout<<a[i]<<" ";
+    }
+
+    printf("Execution time: %f seconds\n", end_time - start_time);
+
+return 0;
+}
